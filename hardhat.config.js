b642e8f5c0b5be6440dfe9ae995bdee6da19bde2
jasonlhy <jasonlhy@gmail.com>
2021-07-26 02:44:28 +0800

    run test

--- hardhat.config.js
+++ hardhat.config.js
@@ -15,7 +15,7 @@ require("./tasks/block-number")
 require("./tasks/random-number-consumer")
 require("./tasks/price-consumer")
 require("./tasks/api-consumer")
-
+require('solidity-coverage')
 
 require('dotenv').config()
 
@@ -28,7 +28,8 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API k
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "your private key"
 
 module.exports = {
-    defaultNetwork: "hardhat",
+    // defaultNetwork: "hardhat",
+    defaultNetwork: "kovan",
     networks: {
         hardhat: {
             // // If you want to do some forking, uncomment this
